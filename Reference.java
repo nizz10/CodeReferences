50fa82d3170771408e223512ff8f4f22406a2e9a
nizz10 <niz10@pitt.edu>
2017-02-08 02:43:14 -0500

    Update Reference.java
    Added problem description and changed indentation.

--- Reference.java
+++ Reference.java
@@ -1,3 +1,18 @@
+/* A problem from codeforces:
+For a positive integer n let's define a function f:
+
+f(n) =  - 1 + 2 - 3 + .. + ( - 1)nn
+
+Your task is to calculate f(n) for a given integer n.
+
+Input
+The single line contains the positive integer n (1 ≤ n ≤ 1015).
+
+Output
+Print f(n) in a single line.
+*/
+
+
 import java.util.*;
 import java.math.*;
 
